64e49f7d89ad3922c322eee6809299bf007fc064
roman7373 <rra83ul@mail.ru>
2021-10-15 23:49:56 +0400

    отредактировал поля и модели

--- bboard/models.py
+++ bboard/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 
-#объявим модель Bb представляющую объявление с полями..
+# объявим модель Bb представляющую объявление с полями..
+
+
 class Bb(models.Model):
     # ..заголовок объявления с названием продаваемого товара:
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, verbose_name='Товар')
     # сам текст объявления, описание товара:
-    content = models.TextField(null=True, blank=True)
+    content = models.TextField(null=True, blank=True, verbose_name='Описание')
     # цена:
-    price = models.FloatField(null=True, blank=True)
+    price = models.FloatField(null=True, blank=True, verbose_name='Цена')
     # дата публикации
-    published = models.DateTimeField(auto_now_add=True, db_index=True)
+    published = models.DateTimeField(
+        auto_now_add=True, db_index=True, verbose_name='Опубликовано'
+    )
+
+    class Meta:
+        verbose_name_plural = 'Объявления'
+        verbose_name = 'Объявление'
+        ordering = ['-published']
