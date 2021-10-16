b5e1e79e4f219b87bd45705f3fd8f49afeff94b2
roman7373 <rra83ul@mail.ru>
2021-10-16 18:43:33 +0400

    создал форму для ввода объявлений

--- bboard/views.py
+++ bboard/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
-
+from .forms import BbForm
 from .models import Bb
 from .models import Rubric
 
@@ -20,3 +22,14 @@ def by_rubric(request, rubric_id):
         'current_rubric': current_rubric
     }
     return render(request, 'bboard/by_rubric.html', context)
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics']=Rubric.objects.all()
+        return context    
+            
