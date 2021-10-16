58731f135615dac4658c0f922189f6cfbe1001d1
roman7373 <rra83ul@mail.ru>
2021-10-16 15:14:35 +0400

    создал панель навигации со списком рубрик

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from bboard.views import index
+from bboard.views import index, by_rubric
 
 urlpatterns = [
+    path('<int:rubric_id>/', by_rubric),
     path('', index),
 ]
